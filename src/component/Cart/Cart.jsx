f4c113cc05fbd27e6161c769f5756fc9861884bb
sagorforu <freelancersagor18@gmail.com>
2023-04-07 03:42:05 +0600

    cart add and remove product

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const Cart = () => {
+const Cart = ({ cart, handleRemoveFromCart }) => {
     return (
         <div>
-            <h2>Order summary</h2>
+            <h2>Order summary: {cart.length}</h2>
+            {
+                cart.map(tshirt => <p>{tshirt.name} <button
+                    key={tshirt._id}
+                    onClick={() => handleRemoveFromCart(tshirt._id)}
+                >x</button></p>)
+            }
         </div>
     );
 };
